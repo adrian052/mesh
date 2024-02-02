2395424be3e9dbe17c437ac58dc2ce107512b1d8
SIDANWhatever <wongkahinhinson@gmail.com>
2024-02-03 01:30:43 +0800

    fix: fix typo

--- packages/react/src/hooks/useAssets.ts
+++ packages/react/src/hooks/useAssets.ts
@@ -3,19 +3,15 @@ import { WalletContext } from '@mesh/contexts';
 import type { Asset } from '@meshsdk/core';
 
 export const useAssets = () => {
-  const [asstes, setAsstes] = useState<Asset[]>();
-  const {
-    hasConnectedWallet,
-    connectedWalletName,
-    connectedWalletInstance,
-  } = useContext(WalletContext);
+  const [assets, setAssets] = useState<Asset[]>();
+  const { hasConnectedWallet, connectedWalletName, connectedWalletInstance } =
+    useContext(WalletContext);
 
   useEffect(() => {
     if (hasConnectedWallet) {
-      connectedWalletInstance.getAssets()
-        .then(setAsstes);
+      connectedWalletInstance.getAssets().then(setAssets);
     }
   }, [connectedWalletName]);
 
-  return asstes;
+  return assets;
 };
